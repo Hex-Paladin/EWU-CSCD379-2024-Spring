781009a0c6341e6aab5a65a68ccd7011092c5682
Bryan Snyder <bsnyder13@ewu.edu>
2024-04-21 11:32:14 -0700

    Update app.vue

--- wordle-web/app.vue
+++ wordle-web/app.vue
@@ -1,37 +1,13 @@
 <template>
   <NuxtLayout>
     <v-app>
-      <v-app-bar color="primary" :elevation="2">
-        <template v-slot:prepend>
-          <v-icon color="#FFFFFF"> mdi-flower-poppy </v-icon>
-        </template>
-
-        <v-app-bar-title>Bryan's Wordle Game</v-app-bar-title>
-
-        <v-btn @click="router.push('/')">Home</v-btn>
-        <v-btn @click="router.push('/about')">About</v-btn>
-        <v-btn icon="mdi-theme-light-dark" @click="toggleTheme" />
-        <v-btn icon="mdi-cog" @click="showSettingsDialog = true" />
-        <SettingsDialogue v-model="showSettingsDialog" />
-      </v-app-bar>
-      <v-main>
-        <NuxtPage />
-      </v-main>
+      <v-btn @click="router.push('/')">Home</v-btn>
+      <v-btn @click="router.push('/game')">Game</v-btn>
+      <NuxtPage />
     </v-app>
   </NuxtLayout>
-
 </template>
 
 <script setup lang="ts">
-import { useTheme } from "vuetify/lib/framework.mjs";
-const router = useRouter();
-const theme = useTheme();
-const showSettingsDialog = ref(false);
-function toggleTheme() {
-  if (theme.global.name.value === "light") {
-    theme.global.name.value = "dark";
-  } else {
-    theme.global.name.value = "light";
-  }
-}
+const router = useRouter()
 </script>
