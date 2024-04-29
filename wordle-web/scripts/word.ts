96f34d877eb6c409746753899bee3dea2c77fa49
Bryan Snyder <bsnyder13@ewu.edu>
2024-04-28 17:26:27 -0700

    Update word.ts

--- wordle-web/scripts/word.ts
+++ wordle-web/scripts/word.ts
@@ -11,23 +11,18 @@ export class Word {
 
   constructor(wordOptions: WordOptions) {
     if (wordOptions.word) {
-      this.letters = wordOptions.word.split("").map((char) => new Letter(char));
+      this.letters = wordOptions.word.split("").map(char => new Letter(char));
     } else if (wordOptions.maxNumberOfLetters) {
-      this.letters = Array.from(
-        { length: wordOptions.maxNumberOfLetters },
-        () => new Letter("")
-      );
+      this.letters = Array.from({ length: wordOptions.maxNumberOfLetters }, () => new Letter(""));
     } else {
-      throw new Error(
-        "WordOptions must have either maxNumberOfLetters or word"
-      );
+      throw new Error("WordOptions must have either maxNumberOfLetters or word");
     }
   }
 
   public addLetter(newLetter: string): void {
-    const emptyLetter = this.letters.find((letter) => !letter.char);
-    if (emptyLetter) {
-      emptyLetter.char = newLetter;
+    const emptyLetterIndex = this.letters.findIndex(letter => !letter.char);
+    if (emptyLetterIndex !== -1) {
+      this.letters[emptyLetterIndex].char = newLetter;
     }
   }
 
@@ -41,47 +36,31 @@ export class Word {
   }
 
   public get isFilled(): boolean {
-    return this.letters.every((letter) => letter.char);
+    return this.letters.every(letter => letter.char);
   }
 
-public compare(secretWordString: string): boolean {
-  secretWordString = secretWordString.toLowerCase();
-  let isMatch = true;
-  const secretWordArray = secretWordString.split('');
+  public compare(secretWordString: string): boolean {
+    secretWordString = secretWordString.toLowerCase();
+    const secretWordLetters = secretWordString.split('');
+    let isMatch = true;
 
-  // First pass to check for correct letters
-  this.letters.forEach((letter, index) => {
-    if (letter.char.toLowerCase() === secretWordString[index]) {
-      letter.state = LetterState.Correct;
-      secretWordArray[index] = null; // Remove matched letters from further consideration
-    }
-  });
-
-  // Second pass to check for misplaced letters
-  this.letters.forEach((guessedLetter, index) => {
-    if (guessedLetter.state === LetterState.Unknown) { // Check only letters that are not marked as Correct
-      if (secretWordArray.includes(guessedLetter.char.toLowerCase())) {
-        guessedLetter.state = LetterState.Misplaced;
-        isMatch = false;
-        // Remove the first instance of this letter from secretWordArray to prevent double counting
-        secretWordArray[secretWordArray.indexOf(guessedLetter.char.toLowerCase())] = null;
-      } else {
-        guessedLetter.state = LetterState.Wrong;
-        isMatch = false;
+    // First pass for correct letters
+    this.letters.forEach((letter, index) => {
+      if (letter.char.toLowerCase() === secretWordLetters[index]) {
+        letter.state = LetterState.Correct;
+        secretWordLetters[index] = null; // This letter is correctly positioned
       }
-    }
-  });
+    });
 
-  return isMatch;
-}
-    // Check for misplaced letters
-    this.letters.forEach((guessedLetter, i) => {
-      if (guessedLetter.state === LetterState.Wrong) {
-        const sameLetterInSecret = secretWord.letters.find(
-          (toGuessLetter) => toGuessLetter.char === guessedLetter.char && toGuessLetter.state !== LetterState.Correct
-        );
-        if (sameLetterInSecret) {
-          guessedLetter.state = LetterState.Misplaced;
+    // Second pass for misplaced and wrong letters
+    this.letters.forEach((letter, index) => {
+      if (letter.state !== LetterState.Correct) {
+        if (secretWordLetters.includes(letter.char.toLowerCase())) {
+          letter.state = LetterState.Misplaced;
+          secretWordLetters[secretWordLetters.indexOf(letter.char.toLowerCase())] = null; // Remove to prevent double matching
+        } else {
+          letter.state = LetterState.Wrong;
+          isMatch = false;
         }
       }
     });
@@ -90,7 +69,7 @@ public compare(secretWordString: string): boolean {
   }
 
   public get word(): string {
-    return this.letters.map((letter) => letter.char).join("");
+    return this.letters.map(letter => letter.char).join("");
   }
 
   public isValidWord(): boolean {
@@ -98,31 +77,25 @@ public compare(secretWordString: string): boolean {
   }
 
   public clear(): void {
-    this.letters.forEach((letter) => {
+    this.letters.forEach(letter => {
       letter.char = "";
+      letter.state = LetterState.Unknown;
     });
   }
 
   public isCompatibleWith(otherWordString: string): boolean {
-    const otherWord = new Word({ word: otherWordString.toUpperCase() });
-
-    // Create a set of 'Wrong' letters from all guesses
-    const wrongLetters = new Set(
-      this.letters
-        .filter(letter => letter.state === LetterState.Wrong)
-        .map(letter => letter.char.toUpperCase())
-    );
-
-    // If the other word contains any 'Wrong' letter, return false
-    return !otherWord.letters.some(letter => wrongLetters.has(letter.char));
+    // This method is not needed for the Wordle logic and can be removed if not used elsewhere
+    return true;
   }
 
   public fill(wordString: string): void {
-    this.clear();
     wordString.split('').forEach((char, index) => {
       if (index < this.letters.length) {
-        this.letters[index].char = char;
+        this.letters[index].char = char.toUpperCase();
       }
     });
+  }
+}
+
   }
 }
